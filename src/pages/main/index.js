6e260a522073298f6931776f7c6272b8118bf12e
Serasmi <serasmi@yandex.ru>
2020-04-24 21:05:10 +0300

    Configure main page.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Button from '../../components/Button';
+import './main.scss';
 
-const Main = () => {
-  return <div>Main component</div>;
+const Main = ({ image = {} }) => {
+  const handleClick = () => {
+    if (image.url) {
+      console.log('Saved!');
+    } else {
+      console.log('Loaded!');
+    }
+  };
+
+  return (
+    <div className="main">
+      {image.url ? <img src={image.url} alt={image.title} /> : <div className="hint">Изображение отсутствует</div>}
+      <Button onClick={handleClick}>Загрузить</Button>
+    </div>
+  );
 };
 
 export default Main;
